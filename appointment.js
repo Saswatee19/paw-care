025c2ede8743511120b4744bac4e5bd284453c95
19sas <19.saswatee@gmail.com>
2026-06-24 22:22:35 +0530

    submission validation for contact us page
    added logo for all connecting pages
    
    fixing animations and quick-info section for landing page

--- appointment.js
+++ appointment.js
@@ -36,3 +36,40 @@ let preloader = document.querySelector(".preloader");
 setTimeout(()=>{
     preloader.classList.add('preloader-hidden');
 }, 2000)
+
+// form validation
+let nm = document.querySelector("#fullName");
+let phn = document.querySelector("#phone");
+let petName = document.querySelector("#petName");
+
+form.addEventListener('submit', (dets)=>{
+    dets.preventDefault();
+    let isValid = true;
+
+    if (nm.value.length <= 2) {
+        document.querySelector(".error1").style.display = "block";
+        isValid = false;
+    }
+    
+    if (phn.value.length != 10) {
+        document.querySelector(".error2").style.display = "block";
+        isValid = false;
+    }
+
+    if (petName.value.length < 3) {
+        document.querySelector(".error3").style.display = "block";
+        isValid = false;
+    }
+
+    
+    if (isValid) {
+        let confirmBox = document.querySelector(".confirm");
+        confirmBox.style.display = "flex";
+        
+        setTimeout(() => {
+            confirmBox.style.display = "none";
+        }, 5000);
+
+        form.reset();
+    }
+});
